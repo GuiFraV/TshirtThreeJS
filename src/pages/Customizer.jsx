5cf7289a6f110b167604dde7adf26b81f2c6a931
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-06 10:42:06 +0200

    add pickers logic

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -11,8 +11,37 @@ import { fadeAnimation, slideAnimation } from '../config/motion';
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
+  
 
   const snap = useSnapshot(state);
+
+  const [file, setFile] = useState('');
+
+  const [prompt, setPrompt] = useState('');
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+
+  const [activeEditorTab, setActiveEditorTab] = useState('');
+
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  const generateTabContent = () => {
+    switch(activeEditorTab) {
+      case 'colorpicker':
+        return <ColorPicker />;
+      case 'filepicker':
+        return <FilePicker />;
+      case 'aipicker':
+        return <AIPicker />;
+      default: 
+        return null;
+    }
+  }
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -24,15 +53,17 @@ const Customizer = () => {
           >
               <div className='flex items-center min-h-screen'>
                 <div className='editortabs-container tabs'>
-                  {EditorTabs.map((tab, index) => (
+                  {EditorTabs.map((tab) => (
                     <Tab 
                       key={tab.name}
                       tab={tab}
-                      handleClick={() => {}}
+                      handleClick={() => setActiveEditorTab(tab.name)}
                     />
                   ))}
-                </div>
 
+                  {generateTabContent()}
+
+                </div>
               </div>
           </motion.div>
           <motion.div
